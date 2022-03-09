b39b6e00acf56b9c2d0f14f97949b5f4cd4c5c5c
hartaithan <hartaithan@gmail.com>
2022-03-09 23:38:40 +0900

    add preview page layout

---
+++ src/components/PreviewItem.vue
@@ -0,0 +1,53 @@
+<template>
+  <p :class="classObject">{{ props.value }}</p>
+</template>
+
+<script setup lang="ts">
+import { computed, defineProps } from 'vue'
+
+const props = defineProps({
+  type: {
+    type: String,
+    required: false,
+  },
+  value: {
+    type: String,
+    required: true,
+  },
+  background: {
+    type: Boolean,
+    required: false,
+  },
+});
+
+const classObject = computed(() => ({
+  'preview-item__parent': props.type === 'parent',
+  'preview-item__child': props.type === 'child',
+  background: props.background
+}))
+</script>
+
+<style scoped lang="scss">
+p {
+  color: #111111;
+  font-weight: bold;
+  font-size: 16px;
+  line-height: 24px;
+  width: fit-content;
+  &.background {
+    padding: 10px 20px;
+    background: #f1f1f1;
+    border-radius: 5px;
+  }
+}
+.preview-item {
+  &__parent {
+    margin-bottom: 60px;
+  }
+  &__child {
+    &:not(:last-child) {
+      margin-bottom: 20px;
+    }
+  }
+}
+</style>
--- src/views/Preview.vue
+++ src/views/Preview.vue
@@ -1,3 +1,44 @@
 <template>
-  <div class="preview">Preview</div>
+  <div class="preview">
+    <h2>Персональные данные</h2>
+    <PreviewItem type="parent" :value="getValue(parent)" />
+    <h2>Дети</h2>
+    <PreviewItem
+      type="child"
+      v-for="child in childrens"
+      :key="child.id"
+      :value="getValue(child)"
+      background
+    />
+  </div>
 </template>
+
+<script setup lang="ts">
+import { useStore } from '@/store';
+import { computed } from 'vue';
+import PreviewItem from '@/components/PreviewItem.vue';
+import { IChildren, IParent } from '@/models/storeModel';
+
+const store = useStore();
+
+const parent = computed(() => store.state.parent)
+const childrens = computed(() => store.state.childrens)
+
+const getValue = (value: IParent | IChildren) => {
+  return `${value.name}, ${value.age}`
+}
+</script>
+
+<style scoped lang="scss">
+.preview {
+  background: transparent;
+}
+h2 {
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 24px;
+  color: #111111;
+  margin-bottom: 20px;
+}
+</style>
+
